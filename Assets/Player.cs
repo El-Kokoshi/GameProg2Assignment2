299dd8ee07b2528f9df879bd6fef7c6d6623f81d
raushan2ashish <raushan22@live.com>
2025-04-07 13:12:54 -0400

    Added Player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+
+
+        // Player rotation toward mouse cursor
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 rotatedirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = rotatedirection;
+
+
+
+        //Player movement
+         Vector3 direction = Vector3.zero;
+         if (Input.GetKey(KeyCode.W))
+         {
+             direction += Vector3.up;
+         }
+         if (Input.GetKey(KeyCode.S))
+         {
+             direction += Vector3.down;
+         }
+         if (Input.GetKey(KeyCode.A))
+         {
+             direction += Vector3.left;
+         }
+         if (Input.GetKey(KeyCode.D))
+         {
+             direction += Vector3.right;
+         }
+         transform.position += direction * 5.0f * Time.deltaTime;
+    }
+}
